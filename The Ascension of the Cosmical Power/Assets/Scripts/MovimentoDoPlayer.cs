feacd3f513ad775b84d5811d002fadfb0e310421
Santiagobbboff <79426542+Santiagobbboff@users.noreply.github.com>
2022-07-15 23:40:36 +0100

    Update 2
    Salto adicionado e movimento agr é 99% funcional

--- The Ascension of the Cosmical Power/Assets/Scripts/MovimentoDoPlayer.cs
+++ The Ascension of the Cosmical Power/Assets/Scripts/MovimentoDoPlayer.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimentoDoPlayer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class MovimentoDoPlayer : MonoBehaviour{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
 
     // Update is called once per frame
-    void Update()
+    void Update(){
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        }
+
+    void FixedUpdate ()
     {
-        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
